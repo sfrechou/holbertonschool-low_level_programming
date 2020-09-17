e6c63d2a6f063ab08df2ab766b7bef5142529dbf
Soledad Frechou <solefrechou@hotmail.com>
2020-09-17 16:17:08 +0000

    Add file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char abc = 'a';
+
+for (abc = 'a'; abc <= 'z'; abc++)
+{
+putchar (abc);
+}
+
+putchar ('\n');
+
+return (0);
+}
