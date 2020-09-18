f5a08a6f006e5fdd78253f62171b135f82fb4d37
Soledad Frechou <solefrechou@hotmail.com>
2020-09-18 00:16:32 +0000

    Add file 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,22 +11,22 @@ int num1;
 int num2;
 int num3;
 int num4;
-for (num1 = 48; num1 <= 57; num1++)
+for (num1 = '0'; num1 <= '9'; num1++)
 {
-for (num2 = 48; num2 <= 57; num2++)
+for (num2 = '0'; num2 <= '9'; num2++)
 {
-for (num3 = 48; num3 <= 57; num3++)
+for (num3 = '0'; num3 <= '9'; num3++)
 {
-for (num4 = 48; num4 <= 57; num4++)
+for (num4 = '0'; num4 <= '9'; num4++)
 {
-if ((num1 + num2) != (num3 + num4) && (num1 + num2) < (num3 + num4))
+if (num1 < num2 || (num1 == num3 && num2 < num4))
 {
 putchar (num1);
 putchar (num2);
 putchar (' ');
 putchar (num3);
 putchar (num4);
-if ((num1 + num2) == 113 && (num3 + num4) == 114)
+if ((num1 + num2) == '17' && (num3 + num4) == '18')
 {}
 else
 {
