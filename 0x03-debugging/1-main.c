212b4b42e1247377e0b720d0e8d107616013c63b
Soledad Frechou <solefrechou@hotmail.com>
2020-09-22 18:08:04 +0000

    Add file 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,13 +10,14 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
+/*
+ *i = 0;
 
-i = 0;
-
-while (i < /*1*/0)
-{
-putchar(i);
-}
+ *while (i < 0)
+ *{
+ *putchar(i);
+ *}
+ */
 
 printf("Infinite loop avoided! \\o/\n");
 
