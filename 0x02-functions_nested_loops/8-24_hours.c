0484934e4d32f9e9c9a51c56224f63624f5cb3d4
Soledad Frechou <solefrechou@hotmail.com>
2020-09-22 21:13:55 +0000

    Add file 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,36 +1,37 @@
 #include "holberton.h"
 
 /**
- * _abs - writes the character c to stdout
- * 
+ * jack_bauer - writes the character c to stdout
+ *
  *
  * Return: On success 0.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void jack_bauer(void)
 {
-  int a, b, c, d, tope;
-  tope = 57;
-  for (a = 48; a <= 50; a++)
-    {
-      for (b = 48; b <= tope; b++)
-	{
-	  for (c = 48; c <= 53; c++)
-	    {
-	      for (d = 48; d <= 57; d++)
-		{
-		  if (a == 50)
-	    {
-	      tope = 51;
-	    }
-	  _putchar(a);
-		  _putchar(b);
-		  _putchar(':');
-		  _putchar(c);
-		  _putchar(d);
-		  _putchar('\n');
-		}
-	    }
-	}
-    }
+int a, b, c, d, tope;
+
+tope = 57;
+for (a = 48; a <= 50; a++)
+{
+for (b = 48; b <= tope; b++)
+{
+for (c = 48; c <= 53; c++)
+{
+for (d = 48; d <= 57; d++)
+{
+if (a == 50)
+{
+tope = 51;
+}
+_putchar(a);
+_putchar(b);
+_putchar(':');
+_putchar(c);
+_putchar(d);
+_putchar('\n');
+}
+}
+}
+}
 }
