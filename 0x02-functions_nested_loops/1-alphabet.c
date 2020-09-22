116a6d59381e497153a8e2d7a93f0603d945848d
Soledad Frechou <solefrechou@hotmail.com>
2020-09-22 20:14:53 +0000

    Add file 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 char abc;
-for( abc = 'a'; abc <= 'z'; abc++)
+for (abc = 'a'; abc <= 'z'; abc++)
 {
-_putchar(abc);
+_putchar (abc);
 }
-_putchar('\n');
+_putchar ('\n');
 }
