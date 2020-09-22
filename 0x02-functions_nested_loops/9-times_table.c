2aa4a054dd21f952b672896986af6e2726a5d704
Soledad Frechou <solefrechou@hotmail.com>
2020-09-22 20:07:46 +0000

    Add file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _putchar - writes the character c to stdout
+ * times_table - writes the character c to stdout
  * : The character to print
  *
  * Return: On success 1.
@@ -9,47 +9,44 @@
  */
 void times_table(void)
 {
-  int a, b, c, d, e;
-  for (a = 0; a <= 9; a++)
-    {
-      for (b = 0; b <= 9; b++)
-	{
-	  c = a * b;
-	  d = c / 10;
-	  e = c % 10;
-	  
-	  if (b == 9 && d != 0)
-	    {
-	      _putchar (' ');
-	      _putchar (d + '0');
-	      _putchar (e + '0');
-	    }
-else
-	  if (b == 0)
-	    {
-	      _putchar (d + '0');
-	      _putchar (',');
-	}
-	  else
-	    	  if (d == 0)
-	    {
-	      _putchar (' ');
-	  _putchar (' ');
-	  _putchar (e + '0');
-	  if (b != 9 && d == 0)
-	    {
-	  _putchar (',');
-	    }
-	    }
-	  else
-	    {
-	      _putchar (' ');
-	      _putchar (d + '0');
-	      _putchar (e + '0');
-	      _putchar (',');
-	    }
-	}
-      _putchar ('\n');
-    }
+int a, b, c, d, e;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+d = c / 10;
+e = c % 10;
 
+if (b == 9 && d != 0)
+{
+_putchar (' ');
+_putchar (d + '0');
+_putchar (e + '0');
+}
+else if (b == 0)
+{
+_putchar (d + '0');
+_putchar (',');
+}
+else if (d == 0)
+{
+_putchar (' ');
+_putchar (' ');
+_putchar (e + '0');
+if (b != 9 && d == 0)
+{
+_putchar (',');
+}
+}
+else
+{
+_putchar (' ');
+_putchar (d + '0');
+_putchar (e + '0');
+_putchar (',');
+}
+}
+_putchar ('\n');
+}
 }
