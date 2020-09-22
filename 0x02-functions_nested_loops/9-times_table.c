5260cead6163d6146c732b9f2c1b79527c65da15
Soledad Frechou <solefrechou@hotmail.com>
2020-09-22 00:30:25 +0000

    Add files 8-9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+void print(int multi);
+/**
+ * _putchar - writes the character c to stdout
+ * : The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void times_table(void)
+{
+
+
+}
+void print(int multi)
+{
+
+
+}
