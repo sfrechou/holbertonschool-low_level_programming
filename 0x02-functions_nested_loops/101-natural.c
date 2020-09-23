17fccba38cce2ac920ab5b2fc0c224c9bbc15ce0
Soledad Frechou <solefrechou@hotmail.com>
2020-09-23 19:32:00 +0000

    Add file 101 fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include "holberton.h"
+void natural(void);
+/**
+ * main - writes the character c to stdout
+ * : The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+natural();
+return (0);
+}
 
 /**
  * natural - writes the character c to stdout
