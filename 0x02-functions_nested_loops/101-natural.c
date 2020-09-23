432e23f2dcf430d6219dd600ad160bedf7f3bb93
Soledad Frechou <solefrechou@hotmail.com>
2020-09-23 19:09:24 +0000

    Add file 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * natural - writes the character c to stdout
+ * : The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int natural(void)
+{
+int sum = 0;
+int a;
+for (a = 0; a < 1024; a++)
+{
+if ((a % 3 == 0) || (a % 5 == 0))
+{
+sum += a;
+}
+}
+printf("%d\n", sum);
+}
