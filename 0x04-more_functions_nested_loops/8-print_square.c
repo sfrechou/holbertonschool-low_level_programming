a2cbd95b47d3352631c235d27bd9f6a71e3427a8
Soledad Frechou <solefrechou@hotmail.com>
2020-09-24 19:34:57 +0000

    Add file 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_square - Entry point
+ * @size: d
+ *
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+int a, b;
+a = size;
+b = size;
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+{
+_putchar (35);
+}
+_putchar ('\n');
+}
+if (size == 0)
+{
+_putchar ('\n');
+}
+}
