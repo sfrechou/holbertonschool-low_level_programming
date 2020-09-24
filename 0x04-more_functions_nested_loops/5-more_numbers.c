c02d2907421298f5fd7a6cf923f0a0bd99adf9f5
Soledad Frechou <solefrechou@hotmail.com>
2020-09-24 15:26:29 +0000

    Add file 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,18 @@
  */
 void more_numbers(void)
 {
-  int n, a;
- 
+int n, a;
 
-  for (n = 0; n <= 9; n++)
-    {
-      for (a = 0; a <= 14; a++)
-	{
-	  if (a > 9)
-	    {
-	      _putchar (49);
-	    }	  
-	  _putchar ((a % 10) + '0'); 
-	}
-      _putchar ('\n');
-    }
- }
+for (n = 0; n <= 9; n++)
+{
+for (a = 0; a <= 14; a++)
+{
+if (a > 9)
+{
+_putchar (49);
+}	  
+_putchar ((a % 10) + '0'); 
+}
+_putchar ('\n');
+}
+}
