b45621480a96e28f9db64c72a7bf813c6f5a19b2
Soledad Frechou <solefrechou@hotmail.com>
2020-09-24 17:06:14 +0000

    Add file 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_line - Entry point
+ * @n: d
+ *
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+int a;
+if (n != 0 || n > 0)
+{
+for (a = 0; a <= n;  a++)
+{
+_putchar (95);
+}
+_putchar ('\n');
+}
+else
+{
+_putchar ('\n');
+}
+}
