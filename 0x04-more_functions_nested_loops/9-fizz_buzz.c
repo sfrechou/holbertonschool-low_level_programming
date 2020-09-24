043669c8557375a5145dc2cc54b9f8ef720f615c
Soledad Frechou <solefrechou@hotmail.com>
2020-09-24 20:27:34 +0000

    Add file 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+
+for (a = 1; a <= 100; a++)
+{
+if ((a % 3 == 0) && (a % 5 == 0))
+{
+printf("FizzBuzz ");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz ");
+}
+else if ((a % 5 == 0) && (a != 100))
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", a);
+}
+if (a == 100)
+{
+printf("Buzz\n");
+}
+}
+return (0);
+}
