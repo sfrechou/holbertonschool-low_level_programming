6fb7708778189b2bac22fde8bd4a88d84d07f486
Soledad Frechou <solefrechou@hotmail.com>
2020-09-29 13:28:14 +0000

    Add file 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,4 +8,31 @@
  */
 void rev_string(char *s)
 {
-int 
+int length, b;
+char *start, *end, temp;
+
+length = 0;
+
+while (*(s + length) != '\0')
+{
+length++;
+}
+
+start = s;
+end = s;
+
+for (b = 0; b < (length - 1); b++)
+{
+end++;
+}
+
+for (b = 0; b < (length / 2); b++)
+{
+temp = *end;
+*end = *start;
+*start = temp;
+
+start++;
+end--;
+}
+}
