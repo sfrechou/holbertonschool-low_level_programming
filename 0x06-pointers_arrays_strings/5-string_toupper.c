0a5271741154eb204927a656c96b34c858cd0e97
Soledad Frechou <solefrechou@hotmail.com>
2020-10-01 15:55:02 +0000

    Add file 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 
 /**
- * _string_toupper - Entry point
+ * string_toupper - Entry point
  * @p: df
- * 
+ *
  *
  * Return: Always 0 (Success)
  */
 char *string_toupper(char *p)
 {
-  int i;
-  
-  for (i = 0; p[i] != '\0'; i++)
-    {
-      if (p[i] >= 97 && p[i] <= 122)
-	{
-	  p[i] = p[i] - 32;
-	}
-    }
-  return (p);
+int i;
+
+for (i = 0; p[i] != '\0'; i++)
+{
+if (p[i] >= 97 && p[i] <= 122)
+{
+p[i] = p[i] - 32;
+}
+}
+return (p);
 }
