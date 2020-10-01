2aaf86e9852077fee1cab2d6a95a50a569d1a881
Soledad Frechou <solefrechou@hotmail.com>
2020-10-01 15:45:59 +0000

    Add file 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
+int d, s;
 
-  int d, s;
+for (d = 0; dest[d] != '\0'; d++)
+{
+}
 
-  for (d = 0; dest[d] != '\0'; d++)
-    {
-    }
-
-  for (s = 0; src[s] != '\0'; s++)
-    {
-      dest[d + s] = src[s];
-    }
-  return (dest);
-} 
+for (s = 0; src[s] != '\0'; s++)
+{
+dest[d + s] = src[s];
+}
+return (dest);
+}
