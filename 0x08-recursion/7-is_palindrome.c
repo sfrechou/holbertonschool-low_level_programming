da1b942d0dc0d77b16f44dea70c04995960e946f
Soledad Frechou <solefrechou@hotmail.com>
2020-10-07 15:57:18 +0000

    Add file 7

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+int palindrome(char *s, int first, int last);
+
+/**
+ * palindrome - Entry point
+ * @s: d
+ * @first: n
+ * @last: n
+ *
+ * Return: Always 0 (Success)
+ */
+int palindrome(char *s, int first, int last)
+{
+if (s[first] != s[last])
+{
+return(0);
+}
+else if (s[first] == s[last] && first < last)
+{
+return(palindrome(s, (first + 1), (last - 1)));
+}
+return(1);
+}
+
+/**
+ * is_palindrome - Entry point
+ * @s: d
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int is_palindrome(char *s)
+{
+int last, first;
+first = 0;
+
+for(last = 0; s[last] != '\0'; last++)
+{
+}
+return(palindrome(s, first, last - 1));
+}
