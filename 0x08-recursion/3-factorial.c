91792856cdc75669350ee055096ad1952b6fcec4
Soledad Frechou <solefrechou@hotmail.com>
2020-10-07 10:47:45 +0000

    Add file 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * factorial - Entry point
+ * @n: d
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int factorial(int n)
+{
+if (n == 0)
+{
+return (1);
+}
+else if (n < 0)
+{
+return (-1);
+}
+else
+{
+return (n * factorial(n - 1));
+}
+}
