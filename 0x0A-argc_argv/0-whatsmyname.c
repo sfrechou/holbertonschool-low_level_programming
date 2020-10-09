cd494608a2c179a694a8ce870b540f6bfb8a4610
Soledad Frechou <solefrechou@hotmail.com>
2020-10-09 13:31:26 +0000

    Add files README holberton 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * @argc: d
+ * @argv: d
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
