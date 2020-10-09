6aeb8c68de9786f3115ea48cebea3d48f690ea27
Soledad Frechou <solefrechou@hotmail.com>
2020-10-09 13:35:57 +0000

    Add file 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * @argc: d
+ * @argv: d
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+if (argv[0] != '\0')
+{
+printf("%d\n", argc - 1);
+}
+return (0);
+}
