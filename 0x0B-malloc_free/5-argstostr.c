01d131d2a22e83c5d472f22e146fafe689658957
Soledad Frechou <solefrechou@hotmail.com>
2020-10-14 22:33:04 +0000

    Add file 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - Entry point
+ * @ac: s
+ * @av: s
+ *
+ * Return: Always 0 (Success)
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, cont;
+	char *quote;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k < ac; k++)
+	{
+		for (j = 0; av[k][j] != '\0'; j++)
+		{
+			cont++;
+		}
+	}
+	cont += ac;
+	quote = (char *)malloc(cont * sizeof(char));
+	if (quote == NULL)
+	{
+		free(quote);
+		return (NULL);
+	}
+	i = 0;
+	for (k = 0; k < ac; k++)
+	{
+		for (j = 0; av[k][j] != '\0'; j++, i++)
+		{
+			quote[i] = av[k][j];
+		}
+		quote[i++] = '\n';
+	}
+	return (quote);
+}
