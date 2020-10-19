1352cb4fd639138c6bb8aff70dd9b5fa8509bdea
Soledad Frechou <solefrechou@hotmail.com>
2020-10-19 21:26:15 +0000

    Add file 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - Entry point
+ * @d: j
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
