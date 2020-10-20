78f141ded6e97d098e46f12ffd9a53512f204148
Soledad Frechou <solefrechou@hotmail.com>
2020-10-20 02:44:54 +0000

    Add file 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - Entry point
+ * @d: j
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void free_dog(dog_t *d)
+{
+free(dog2);
+free(dog_t);
+free(d);
+}
