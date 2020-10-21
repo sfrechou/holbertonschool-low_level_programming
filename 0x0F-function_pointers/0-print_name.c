2372ec2ba51d07e7fcc482c0c0b1c7f0ceb18f18
Soledad Frechou <solefrechou@hotmail.com>
2020-10-21 16:20:25 +0000

    Add file 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - Entry point
+ * @name: s
+ * @f: s
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
