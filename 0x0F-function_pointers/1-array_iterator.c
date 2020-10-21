7822086106ab59f032d9712984c123e04d8d0284
Soledad Frechou <solefrechou@hotmail.com>
2020-10-21 16:53:42 +0000

    Add file 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,8 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-	if (size != 0 && array != NULL)
+
+	if (size != 0 && array != NULL && action != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
