7966a39d66616a02db072a0fe32c813b40c20cde
Soledad Frechou <solefrechou@hotmail.com>
2020-10-21 16:48:24 +0000

    Add file 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Entry point
+ * @array: s
+ * @size: s
+ * @action: s
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
