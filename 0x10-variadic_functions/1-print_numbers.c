d16ecaacbcaa89b32c41a39c6a06d41d8470baaf
Soledad Frechou <solefrechou@hotmail.com>
2020-10-23 15:20:01 +0000

    Add file 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Entry point
+ * @separator: s
+ * @n: n
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, numero;
+	va_list mi_lista;
+
+	numero = 0;
+	if (n > 0 && separator != NULL)
+	{
+		va_start(mi_lista, n);
+		for (i = 0; i < n; i++)
+		{
+			numero = va_arg(mi_lista, int);
+			printf("%i", numero);
+			if (i < n - 1)
+			{
+				printf("%s", separator);
+			}
+		}
+		printf("\n");
+	}
+	va_end(mi_lista);
+}
