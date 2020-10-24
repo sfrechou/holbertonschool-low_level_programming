60ee3e546b3b7966bc9d1d3e4353ceab0e5eb0b7
Soledad Frechou <solefrechou@hotmail.com>
2020-10-24 15:26:42 +0000

    Add fix file 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list mi_lista;
 
 	numero = 0;
-	if (n > 0)
+	if (n > 0 || n != 0)
 	{
 		va_start(mi_lista, n);
 		for (i = 0; i < n; i++)
