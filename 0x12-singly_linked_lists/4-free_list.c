e1f9639313f2723e456e5007fd80cd66eb63540e
Soledad Frechou <solefrechou@hotmail.com>
2020-10-29 19:29:15 +0000

    Add file 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -12,9 +12,12 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while ((temp = head) != NULL)
+	while (head != NULL)
 	{
+		temp = head;
 		head = head->next;
+		free(temp->str);
 		free(temp);
 	}
+	free(head);
 }
