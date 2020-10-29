4c394cedfd9cdc20fce14a6e5afb22fc614ef855
Soledad Frechou <solefrechou@hotmail.com>
2020-10-29 18:57:58 +0000

    Add file 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_node_end - function that prints number of elements in list
+ * @head: pointer to list to print
+ * @str: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode = malloc(sizeof(list_t));
+	list_t *last = *head;
+	char *string = strdup(str);
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	newnode->str = string;
+	newnode->len = i;
+	newnode->next = NULL;
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	last->next = newnode;
+	return (newnode);
+}
