f6239c667ef5dd65689f1897930bd01ea7e07199
Soledad Frechou <solefrechou@hotmail.com>
2020-11-04 13:23:52 +0000

    Add fix file 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newnode->next = NULL;
 	temp = (*head);
 
+	if (idx ==0)
+	{
+		newnode->next = temp->next;
+		temp->next = newnode;
+	}
 	for (i = 0; i < idx - 1; i++)
 	{
 		temp = temp->next;
