664d8dbe13ba9b47d1a2c4cca973ce0864b21ccb
Soledad Frechou <solefrechou@hotmail.com>
2020-11-04 12:36:56 +0000

    Add fix file 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,7 +3,7 @@
 /**
  * add_nodeint_end - function that prints number of elements in list
  * @head: pointer to list to print
- * @str: string
+ * @n: n
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
