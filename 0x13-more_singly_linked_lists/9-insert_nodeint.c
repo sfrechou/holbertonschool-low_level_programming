8c9170391bbf371cf7814511a8ebc0c0f26fccf2
Soledad Frechou <solefrechou@hotmail.com>
2020-11-04 13:16:35 +0000

    Add fix file 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		if (temp == NULL)
 		{
-			break;
+			return (NULL);
 		}
 	}
 	if (temp != NULL)
