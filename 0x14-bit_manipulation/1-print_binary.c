81cd0b5a2d6edb2b8355905f4129c9073854f911
Soledad Frechou <solefrechou@hotmail.com>
2020-11-05 18:46:30 +0000

    Add file 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,16 +9,12 @@
  */
 void print_binary(unsigned long int n)
 {
+	unsigned long int m = n;
+
 	if (n > 1)
 	{
+		m = n & 1;
 		print_binary(n >> 1);
 	}
-	if ((n & 1) == 1)
-	{
-		_putchar('1');
-	}
-	else
-	{
-		_putchar('0');
-	}
+	_putchar(m + '0');
 }
