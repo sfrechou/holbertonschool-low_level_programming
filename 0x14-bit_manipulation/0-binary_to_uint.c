263dc6b6b913d902d84b21e4c39ae0eef201c7d3
Soledad Frechou <solefrechou@hotmail.com>
2020-11-05 17:48:06 +0000

    Add fix file 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,13 +13,19 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0;
 
 	if (b == NULL)
-		return 0;
-
-	while (b[i] == '0' || b[i] == '1')
+		return (0);
+	while (b[i] != '\0')
 	{
-		val <<= 1;
-		val += b[i]-'0';
+		if (b[i] == '0' || b[i] == '1')
+		{
+			val <<= 1;
+			val += b[i] - '0';
+		}
+		else
+		{
+			return (0);
+		}
 		i++;
 	}
-	return val;
+	return (val);
 }
