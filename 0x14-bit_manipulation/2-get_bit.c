440dc7f56f6ab19be3e4a196ab6eaf6e4905afc9
Soledad Frechou <solefrechou@hotmail.com>
2020-11-05 19:11:13 +0000

    Add file 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * get_bit - function that prints a list
+ * @n: pointer to list to print
+ * @index: index
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int m = 1;
+
+	m <<= index;
+	if ((m & n) != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (-1);
+}
