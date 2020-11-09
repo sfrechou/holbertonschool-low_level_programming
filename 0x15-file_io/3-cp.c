d6fff4d1be1aa8e977020ebe81ffa733bbc371a5
Soledad Frechou <solefrechou@hotmail.com>
2020-11-09 20:20:51 +0000

    Add file 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,28 +10,49 @@
  */
 int main(int ac, char **av)
 {
-	int fileto, filefrom, rd, closeto,  closefrom, wr;
-	char buf[1024];
+	int fileto, filefrom, rd = 1, closeto,  closefrom, wr;
+	char *buff = malloc(1024 * sizeof(char));
 
+	if (buff == NULL)
+		return (0);
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fileto = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 00664);
-	filefrom = open(av[1], O_RDONLY);
-	rd = read(filefrom, buf, 1024);
-	if (filefrom == -1 || rd == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	wr = write(fileto, buf, rd);
-	if (fileto == -1 || wr == -1)
+	fileto = open(av[2], O_CREAT | O_TRUNC | O_RDWR, 00664);
+	if (fileto == -1)
 	{
 		dprintf(2, "Error: Can't write to file %s\n", av[2]);
 		exit(99);
 	}
+	filefrom = open(av[1], O_RDONLY);
+	if (filefrom == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	while (rd > 0)
+	{
+		rd = read(filefrom, buff, 1024);
+		if (rd == -1)
+		{
+			free(buff);
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		if (rd > 0)
+		{
+			wr = write(fileto, buff, rd);
+			if (wr == -1)
+			{
+				free(buff);
+				dprintf(2, "Error: Can't write to file %s\n", av[2]);
+				exit(99);
+			}
+		}
+	}
+	free(buff);
 	closeto = close(fileto);
 	closefrom = close(filefrom);
 	if (closeto == -1)
