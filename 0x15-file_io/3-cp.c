258e54b4e9ee30228df44eea14c2587cd3b13a3a
Soledad Frechou <solefrechou@hotmail.com>
2020-11-09 19:02:45 +0000

    Add file 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,13 +21,14 @@ int main(int ac, char **av)
 	fileto = open(av[2], O_CREAT | O_TRUNC | O_RDWR, 0664);
 	if (fileto == -1 || av[1] == NULL)
 	{
-		dprintf(2, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		dprintf(2, "Error: Can't write to file %s\n", av[2]);
 		exit(98);
 	}
 	filefrom = open(av[1], O_RDWR, 0664);
 	if (filefrom == -1)
 	{
-		return (-1);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(99);
 	}
 	rd = read(filefrom, buf, 1024);
 	write(fileto, buf, rd);
