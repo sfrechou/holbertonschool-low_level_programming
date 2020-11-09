9ca6d2942dfcfd47e189d4ecde15ab2cf8eadb22
Soledad Frechou <solefrechou@hotmail.com>
2020-11-09 17:34:34 +0000

    Add file 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - function that prints a list
+ * @filename: file to read and write
+ * @text_content: numbers of characters
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, length = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_APPEND | 0_RDWR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		while (text_content[length] != '\0')
+		{
+			length++;
+		}
+		write(fd, text_content, length);
+	}
+	close(fd);
+	return (1);
+}
