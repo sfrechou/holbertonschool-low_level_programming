1d248ce69ace4c6fa983731b6242acb9c22a2925
Soledad Frechou <solefrechou@hotmail.com>
2020-11-09 17:06:09 +0000

    Add file 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * crete_file - function that prints a list
+ * @filename: file to read and write
+ * @text_content: numbers of characters
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, length = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	while (text_content[length] != '\0')
+	{
+		length++;
+	}
+
+	write(fd, text_content, length);
+	return (1);
+}
