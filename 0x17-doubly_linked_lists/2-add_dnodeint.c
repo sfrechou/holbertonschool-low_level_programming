d72fe74d814d26add8138461c0e99c1cc03215b5
Soledad Frechou <solefrechou@hotmail.com>
2020-12-11 17:40:11 +0000

    Add file 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function that prints number of elements in list
+ * @head: pointer to list to print
+ * @n: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode;
+
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->n = n;
+	newnode->next = *head;
+	newnode->prev = NULL;
+	*head = newnode;
+	(*head)->prev = newnode;
+	return (newnode);
+}
