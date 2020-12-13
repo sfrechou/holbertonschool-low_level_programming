0b45d8fc4d5474d46bc4e20eaca10cde49f62e31
Soledad Frechou <solefrechou@hotmail.com>
2020-12-13 23:38:46 +0000

    Add fix file 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,10 +21,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		(*head) = temp->next;
+		(*head)->prev = NULL;
 		free(temp);
 		return (1);
 	}
-	for (i = 0; temp != NULL && i < index - 1; i++)
+	for (i = 0; i < (index - 1); i++)
 	{
 		if (temp == NULL || temp->next == NULL)
 		{
