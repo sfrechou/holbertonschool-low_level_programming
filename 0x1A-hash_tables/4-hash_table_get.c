372ecc12ebe20f60aede516569c45cc2e6254f15
Soledad Frechou <solefrechou@hotmail.com>
2021-01-15 16:50:22 +0000

    Add fix 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,17 +9,27 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *temp;
 	unsigned long int index = 0;
 
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 		return (NULL);
-	while (ht->array[index]->key != key)
+
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+/*	while (ht->array[index]->key != key)
 	{
 		if (ht->array[index]->next != NULL)
 			ht->array[index] = ht->array[index]->next;
 		else
 			return (NULL);
 	}
-	return (ht->array[index]->value);
+	return (ht->array[index]->value);*/
 }
