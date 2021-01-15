d8d989dfa1cc8c72344e8362abe0a65265402b90
Soledad Frechou <solefrechou@hotmail.com>
2021-01-15 23:33:22 +0000

    Add fix .h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -76,7 +76,7 @@ typedef struct shash_table_s
 } shash_table_t;
 
 
-shash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
