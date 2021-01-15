cc7f294b0679cc273dd9b7f68141412592e0c1e2
Soledad Frechou <solefrechou@hotmail.com>
2021-01-15 16:00:01 +0000

    Add

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,14 +19,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (key[0] == '\0')
 		return (0);
+	if (value == NULL)
+		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
 
 	newnode = malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
-	{
 		return (0);
-	}
 	newnode->key = (char *)key;
 	newnode->value = strdup(value);
 	if (ht->array[index] != NULL)
