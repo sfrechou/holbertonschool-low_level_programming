3ff6f9fb439638f3b73d49278ef7e315b8b5a7e6
Soledad Frechou <solefrechou@hotmail.com>
2021-01-15 18:48:57 +0000

    Add fix 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,43 +9,27 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    /*hash_node_t *auxnode, *tempnode;*/
-    hash_node_t *list, *temp;
-    
-    if (ht == NULL)
-        return;
-    /* Free the memory for every item in the table, including the * strings themselves. */
-    for (i = 0; i < ht->size; i++)
-    {
-        list = ht->array[i];
-        while (list != NULL)
-        {
-            temp = list;
-            list = list->next;
-            free(temp->key);
-            free(temp->value);
-            free(temp);
-        }
-    }
-    /* Free the table itself */
-    free(ht->array);
-    free(ht);
+	unsigned long int i;
+	hash_node_t *auxnode, *tempnode;
 
-/*        for (i = 0; i < ht->size; i++)
-        {   
-            if (ht->array[i] != NULL)
-            {    
-                auxnode = ht->array[i];
-                while (auxnode != NULL)
-                {
-                    tempnode = auxnode->next;
-                    free(auxnode);
-                    auxnode = tempnode;
-                }
-                tempnode = auxnode;
-                free(tempnode);
-            }
-        }
-*/
+	if (ht == NULL)
+	{
+		free(ht);
+		return;
+	}
+
+	for (i = 0; i < ht->size; i++)
+	{
+		auxnode = ht->array[i];
+		while (auxnode != NULL)
+		{
+			tempnode = auxnode->next;
+			free(auxnode->key);
+			free(auxnode->value);
+			free(auxnode);
+			auxnode = tempnode;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
