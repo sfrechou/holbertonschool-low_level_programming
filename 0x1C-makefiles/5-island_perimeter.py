8dd93019614bb17a4f87c3a74956dc6dc270f232
Soledad Frechou <solefrechou@hotmail.com>
2021-02-11 23:35:13 +0000

    Add

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,13 +7,17 @@ def island_perimeter(grid):
     for row in range(0, len(grid)):
         for cell in range(0, len(grid[row])):
             if grid[row][cell] == 1:
-                if grid[row - 1][cell] == 0:
+                if row <= 0:
+                    per += 1
+                elif grid[row - 1][cell] == 0:
                     per += 1
                 if row >= (len(grid) - 1):
                     per += 1
                 elif grid[row + 1][cell] == 0:
                     per += 1
-                if grid[row][cell - 1] == 0:
+                if cell <= 0:
+                    per += 1
+                elif grid[row][cell - 1] == 0:
                     per += 1
                 if cell >= (len(grid[row]) - 1):
                     per += 1
